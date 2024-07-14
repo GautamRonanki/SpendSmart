c186cdd272bbefb75d9cb72f02c649719b9ce94c
gautamronanki <gautamronanki1412@gmail.com>
2024-07-14 13:56:13 -0400

    Initial commit

---
+++ SpendSmart/Models/ExpensesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart;
+
+public class ExpensesDbContext : DbContext
+{
+    public DbSet<Expense> Expenses {get; set;}
+    public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options)
+        : base(options)
+    {
+        
+    }
+}
